f3bf87e6b4202770b09516775c66dc57cbc4b328
Kitso <kleshope@yahoo.com>
2018-12-05 14:53:14 +0200

    Add example months to get_filters function doc string

--- BikeShare.py
+++ BikeShare.py
@@ -12,7 +12,7 @@ def get_filters():
 
     Returns:
         (str) city - name of the city to analyze, chicago, washington or new_york_city
-        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) month - name of the month to filter by, January, February, etc. or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
 
